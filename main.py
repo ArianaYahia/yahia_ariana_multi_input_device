0b86debd27a4a1052b4eb948a71b3c31fdef06aa
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2021-01-20 19:14:46 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 while True:
-    if input.temperature(TemperatureUnit.CELSIUS) > 10 or input.sound_level() < 100:
+    if input.temperature(TemperatureUnit.CELSIUS) < 40 or input.sound_level() < 100:
         music.ba_ding.play_until_done()
-    elif input.temperature(TemperatureUnit.CELSIUS) < 10 or input.sound_level() > 100:
+    elif input.temperature(TemperatureUnit.CELSIUS) > 40 or input.sound_level() > 100:
         music.stop_all_sounds()
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 while (true) {
-    if (input.temperature(TemperatureUnit.Celsius) > 10 || input.soundLevel() < 100) {
+    if (input.temperature(TemperatureUnit.Celsius) < 40 || input.soundLevel() < 100) {
         music.baDing.playUntilDone()
-    } else if (input.temperature(TemperatureUnit.Celsius) < 10 || input.soundLevel() > 100) {
+    } else if (input.temperature(TemperatureUnit.Celsius) > 40 || input.soundLevel() > 100) {
         music.stopAllSounds()
     }
     
