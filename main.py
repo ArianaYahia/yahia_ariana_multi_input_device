80c4077be2032935edb723e3b9f35d6bace5f643
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2021-01-21 15:29:27 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
-    if input.temperature(TemperatureUnit.FAHRENHEIT) <= 68 or input.sound_level() > 100:
-        music.magic_wand.play_until_done() and light.show_animation(light.rainbow_animation, 100)
+    if input.temperature(TemperatureUnit.FAHRENHEIT) < 40 or input.sound_level() > 100:
+        light.show_animation(light.rainbow_animation, 100) and  music.magic_wand.play_until_done()
     else: 
         music.stop_all_sounds() and light.clear()
-        
+    
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
-    if (input.temperature(TemperatureUnit.Fahrenheit) <= 68 || input.soundLevel() > 100) {
-        music.magicWand.playUntilDone() && light.showAnimation(light.rainbowAnimation, 100)
+    if (input.temperature(TemperatureUnit.Fahrenheit) < 40 || input.soundLevel() > 100) {
+        light.showAnimation(light.rainbowAnimation, 100) && music.magicWand.playUntilDone()
     } else {
         music.stopAllSounds() && light.clear()
     }
