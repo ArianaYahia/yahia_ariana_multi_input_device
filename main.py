9be7b7a3f493ca340d85be3b50fbd6bfbf6fecd9
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2021-01-20 22:30:13 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 while True:
-    if input.temperature(TemperatureUnit.FAHRENHEIT) < 40 or input.sound_level() > 150:
+    if input.temperature(TemperatureUnit.FAHRENHEIT) <= 40 or input.sound_level() >= 150:
         music.magic_wand.play()
     else:
         music.stop_all_sounds()
-
